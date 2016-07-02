83e121001f0deda5d670ea82805fba002b3bbcbc
karatekid <mikechri@umich.edu>
2016-07-02 10:21:08 -0700

    Causing SEG Fault, need to virtualize S_INIT when running in development

--- src/gpio/stm32f0_gpio.c
+++ src/gpio/stm32f0_gpio.c
@@ -1,6 +1,8 @@
 #include "gpio/stm32f0_gpio.h"
+#include "utils/system_memory.h"
 
 GPIO gpio_create(uint32_t gpio_port) {
-    // Don't do anything yet
-    return NULL;
+    GPIOStruct * value = S_INIT(GPIOStruct, GPIO_A_BASE_ADDRESS);
+    value->MODER = 0x10;
+    return (GPIO) value;
 }
---
+++ src/utils/system_memory.h
@@ -0,0 +1,2 @@
+#define SYSTEM_INIT(type, base_address) ((type *) base_address)
+#define S_INIT SYSTEM_INIT

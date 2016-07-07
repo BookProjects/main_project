1aca13221525cf14caa27cc7cb324c969307bbbb
karatekid <mikechri@umich.edu>
2016-07-07 00:15:05 -0700

    Initialize correct gpio ports

--- src/gpio/stm32f0_gpio.c
+++ src/gpio/stm32f0_gpio.c
@@ -1,7 +1,16 @@
 #include "gpio/stm32f0_gpio.h"
 
+static const uintptr_t GPIOPorts[] = {
+    GPIO_A_BASE_ADDRESS,
+    GPIO_B_BASE_ADDRESS,
+    GPIO_C_BASE_ADDRESS,
+    GPIO_D_BASE_ADDRESS,
+    (uintptr_t) NULL,
+    GPIO_F_BASE_ADDRESS
+};
+
 GPIO gpio_create(uint32_t gpio_port) {
-    return (GPIO) S_INIT(GPIOStruct, GPIO_A_BASE_ADDRESS);
+    return (GPIO) S_INIT(GPIOStruct, GPIOPorts[gpio_port]);
 }
 
 err_t gpio_destroy(GPIO gpio) {

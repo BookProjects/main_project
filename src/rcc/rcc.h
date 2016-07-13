d5bfdc301e739080406c7d3fe8f96ba98ce91bf3
karatekid <mchristen96@gmail.com>
2016-07-12 22:13:20 -0700

    Most likely destroyed

--- src/rcc/rcc.h
+++ src/rcc/rcc.h
@@ -4,4 +4,10 @@
 void rcc_create();
 void rcc_reset();
 
+void rcc_configure_clock(CLOCK_CONFIG config);
+
+void rcc_reset_peripheral(PERIPH peripheral);
+void rcc_enable_peripheral(PERIPH peripheral);
+
+
 #endif

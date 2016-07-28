79c548af7e160d1efa6ba911a9ca3cb10344da1d
karatekid <mchristen96@gmail.com>
2016-07-28 00:09:59 -0700

    make system memory test independent of rcc

--- tests/native/utils/test_system_memory.c
+++ tests/native/utils/test_system_memory.c
@@ -1,6 +1,5 @@
 #include "unity_fixture.h"
 
-#include "rcc/stm32f0_rcc.h"
 #include "utils/system_memory.h"
 #include "mocks/mock_system_memory_internals.h"
 
@@ -10,30 +9,36 @@ TEST_GROUP_RUNNER(SystemMemory) {
     RUN_TEST_CASE(SystemMemory, SetMask);
 }
 
-static RCCStruct test_rcc;
+typedef struct {
+    uint32_t val1,
+             val2;
+} TestStruct;
+
+static TestStruct test_struct;
 
 TEST_SETUP(SystemMemory) {
 }
 
 TEST_TEAR_DOWN(SystemMemory) {
-    test_rcc = (RCCStruct) { 0 };
+    test_struct = (TestStruct) { 0 };
 }
 
 TEST(SystemMemory, SetMask) {
-    RCCStruct * rcc_p= &test_rcc;
+    TestStruct * struct_p = &test_struct;
     UT_PTR_SET(system_write, mock_system_write_impl);
 
-    system_write_Expect(&(test_rcc.AHBENR), BIT(1));
-    S_SM(rcc_p, AHBENR, BIT(1), ON);
+    system_write_Expect(&(test_struct.val1), BIT(1));
+    S_SM(struct_p, val1, BIT(1), ON);
 
-    system_write_Expect(&(test_rcc.AHBENR), BIT(2) | BIT(1));
-    S_SM(rcc_p, AHBENR, BIT(2), ON);
+    system_write_Expect(&(test_struct.val1), BIT(2) | BIT(1));
+    S_SM(struct_p, val1, BIT(2), ON);
 
-    system_write_Expect(&(test_rcc.AHBENR), BIT(2));
-    S_SM(rcc_p, AHBENR, BIT(1), OFF);
+    system_write_Expect(&(test_struct.val1), BIT(2));
+    S_SM(struct_p, val1, BIT(1), OFF);
 
-    system_write_Expect(&(test_rcc.AHBENR), TSC_EN | BIT(2));
-    S_SM(rcc_p, AHBENR, get_peripheral_rcc_bit(TSC), ON);
-    system_write_Expect(&(test_rcc.AHBENR), BIT(2));
-    S_SM(rcc_p, AHBENR, get_peripheral_rcc_bit(TSC), OFF);
+    system_write_Expect(&(test_struct.val1), BIT(3) | BIT(2));
+    S_SM(struct_p, val1, BIT(3), ON);
+    system_write_Expect(&(test_struct.val1), BIT(2));
+    S_SM(struct_p, val1, BIT(3), OFF);
+
 }

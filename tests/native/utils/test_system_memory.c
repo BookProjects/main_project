245f8714198f5812d6105c22399042d967bbd75f
karatekid <mchristen96@gmail.com>
2016-09-05 16:43:01 -0700

    Remove setjmp tests

--- tests/native/utils/test_system_memory.c
+++ tests/native/utils/test_system_memory.c
@@ -3,13 +3,10 @@
 #include "utils/system_memory.h"
 #include "mocks/mock_system_memory_internals.h"
 
-#include <setjmp.h>
-
 TEST_GROUP(SystemMemory);
 
 TEST_GROUP_RUNNER(SystemMemory) {
     RUN_TEST_CASE(SystemMemory, SetMask);
-    RUN_TEST_CASE(SystemMemory, SetJmp);
 }
 
 typedef struct {
@@ -43,20 +40,4 @@ TEST(SystemMemory, SetMask) {
     S_SM(struct_p, val1, BIT(3), ON);
     system_write_Expect(&(test_struct.val1), BIT(2));
     S_SM(struct_p, val1, BIT(3), OFF);
-}
-
-static void test_setjmp(jmp_buf env, int what_to_send) {
-    longjmp(env, what_to_send);
-}
-
-TEST(SystemMemory, SetJmp) {
-    jmp_buf env;
-    int count = 0;
-    int res = setjmp(env);
-    if(count > 4) {
-        return;
-    }
-    TEST_ASSERT_EQUAL(count, res);
-    count ++;
-    test_setjmp(env, count);
 }
